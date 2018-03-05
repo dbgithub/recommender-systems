7cddef4eea09594ed981dc3f30ae62758b4c7b53
Aitor DB <aitor.db@opendeusto.es>
2018-03-05 01:10:12 +0100

    major function or method declaracions defined already. Clear distribution of the business logic. MAIN created. Implementation of 'how_many' started (and possibly finished)

--- nonpersonalized/main.py
+++ nonpersonalized/main.py
@@ -8,13 +8,18 @@ __email__ = "aitor.deblas@ugent.be"
 
 import data
 import utils
+import operator  # used to sort the (key,value) pairs of a dictionary
+
+MOVIE_PICKLE_LOCATION = "movies_pickle_26-02-2018--20-19-52.pkl"
+RATINGS_PICKLE_LOCATION = "ratings_pickle_26-02-2018--17-55-35.pkl"
+
 
 def calculate_simple_association(movieX, movieY):
     """
     Calculates the simple association value for movieX with respect movieY.
     :param movieX: ID of movie X
     :param movieY: ID of movie Y
-    :return:
+    :return: the value computed by the simple association
     """
 
 def calculate_advanced_association(movieX, movieY):
@@ -22,15 +27,56 @@ def calculate_advanced_association(movieX, movieY):
     Calculates the advanced association value for movieX with respect movieY.
     :param movieX: ID of movie X
     :param movieY: ID of movie Y
-    :return:
+    :return: the value computed by the advanced association
     """
 
 def topN_movies_simple_association(movieX_ID):
     """
-    Retrieves a list of movie IDs with the highest simple association value with respect movieX
-    :param movieID:
-    :return:
+    Retrieves a list of movie IDs with the highest simple association value with respect movieX.
+    In case of a tie, the movie with the higher ID is ranked before the movie with lower ID.
+    :param movieX_ID: ID of movie X
+    :return: a list of movie IDs
     """
+
+def topN_movies_advanced_association(movieX_ID):
+    """
+    Retrieves a list of movie IDs with the highest advanced association value with respect movieX.
+    In case of a tie, the movie with the higher ID is ranked before the movie with lower ID.
+    :param movieX_ID: ID of movie X
+    :return: a list of movie IDs
+    """
+
+def topN_most_rated_movies():
+    """
+    Retrieves a list of movie names and another list with their corresponding ratings which are the
+    most rated movies.
+    :return: a list of (most rated) movie names AND a list of their ratings as well. Ordered from BIG to SMALL.
+    """
+    aggregated_ratings = {}
+    top10_movies = []
+    top10_ratings = []
+    # First, let's load the data:
+    movies = data.load_pickle(MOVIE_PICKLE_LOCATION)
+    ratings = data.load_pickle(RATINGS_PICKLE_LOCATION)
+    # Now, we will iterate over all ratings and we will aggregate/count all ratings for every movie:
+    for elem in ratings:
+        if not elem['movieid'] in aggregated_ratings:
+            aggregated_ratings[elem['movieid']] = 1
+        else:
+            aggregated_ratings[elem['movieid']] += 1
+    # print "Num elements (aggregated dictionary): ", len(aggregated_ratings)
+    # 'sorted' function sorts the dictionary from small to big, returns a list:
+    mysorted = sorted(aggregated_ratings.items(), key=operator.itemgetter(1))
+    # We take the last 10 elements from the tail:
+    mysorted = mysorted[-10:]
+    # We reverse the order of the items within the list. Now the first item in the list is the most rated movie ID.
+    mysorted.reverse()
+    # print mysorted
+    # Instead of returning movie IDs, we will return movie NAMES:
+    for elem in mysorted:
+        top10_movies.append(movies[elem[0]]['title'])
+        top10_ratings.append(elem[1])
+    return top10_movies, top10_ratings
 
 def main():
     # Load data and parse it:
@@ -40,15 +86,15 @@ def main():
     # dump_pickle(mymovies, generate_file_name("movies", "pkl"))
     # dump_pickle(myratings, generate_file_name("ratings", "pkl"))
     # Load the pickles (much faster than loading and parsing again the raw data):
-    # mymovies2 = load_pickle("movies_pickle_26-02-2018--17-55-35.pkl") # This pickle has another data structure for Movies.
-    mymovies2 = data.load_pickle("movies_pickle_26-02-2018--20-19-52.pkl")
-    myratings2 = data.load_pickle("ratings_pickle_26-02-2018--17-55-35.pkl")
-    print len(mymovies2)
-    print len(myratings2)
-    print mymovies2['3196']
-    print myratings2[0]
-    # Show topN rated movies:
-    utils.plot_top10_rated_distribution(mymovies2, myratings2)
+    movies_pkl = data.load_pickle(MOVIE_PICKLE_LOCATION)
+    ratings_pkl = data.load_pickle(RATINGS_PICKLE_LOCATION)
+    print len(movies_pkl)
+    print len(ratings_pkl)
+    print movies_pkl['3196']
+    print ratings_pkl[0]
+    # Show top10 rated movies:
+    top10_movies, top10_ratings = topN_most_rated_movies()
+    utils.plot_top10_rated_distribution(top10_movies, top10_ratings)
 
 
 if __name__ == '__main__':
--- nonpersonalized/utils.py
+++ nonpersonalized/utils.py
@@ -2,11 +2,10 @@
 This module is called 'utils' because it owns miscellaneous methods that can come in handy
 in different scenarios throughout the project/application.
 
-For instance, plotting charts.
+For instance, plotting charts, declaration of subroutines used in main, etc.
 """
 
 import matplotlib.pyplot as plt
-import operator  # used to sort the (key,value) pairs of a dictionary
 
 __author__ = "Aitor De Blas Granja"
 __email__ = "aitor.deblas@ugent.be"
@@ -15,35 +14,39 @@ __email__ = "aitor.deblas@ugent.be"
 def plot_top10_rated_distribution(movies, ratings):
     """
     Plots a distribution of top10 rated movies.
-    :param movies: a collection of movies (mainly used for the names, instead of using IDs)
-    :param ratings: a collection of ratings
+    :param movies: ten most rated movies' names.
+    :param ratings: ten most rated movies' ratings
     :return:
     """
-    aggregated_ratings = {}
-    top10_movies = []
-    top10_ratings = []
-    # Now, we will iterate over all ratings and we will aggregate/count all ratings for every movie:
-    for elem in ratings:
-        if not elem['movieid'] in aggregated_ratings:
-            aggregated_ratings[elem['movieid']] = 1
-        else:
-            aggregated_ratings[elem['movieid']] += 1
-    print "Num elements (aggregated dictionary): ", len(aggregated_ratings)
-    # 'sorted' function sorts the dictionary from small to big, returns a list:
-    mysorted = sorted(aggregated_ratings.items(), key=operator.itemgetter(1))
-    # We take the last 10 elements from the tail:
-    mysorted = mysorted[-10:]
-    # We reverse the order of the items within the list:
-    mysorted.reverse()
-    # print mysorted
-    for elem in mysorted:
-        top10_movies.append(movies[elem[0]]['title'])
-        top10_ratings.append(elem[1])
-    print top10_movies
-    print top10_ratings
+    print movies
+    print ratings
     plt.rcParams.update({'figure.autolayout': True})
     plt.figure(figsize=(10, 4))
-    plt.barh(top10_movies, top10_ratings)
+    plt.barh(movies, ratings)
     plt.ylabel("Movies")
     plt.xlabel("Amount of ratings")
     plt.show()
+
+
+def how_many_Z(movieID, ratings):
+    """
+    Computes how many instances (ratings) are done for movieID.
+    It's named: "how_many_Z" because it's valid to calculate the amount of ratings for every movie ID.
+    :param movieID: movie ID from which we want to know how many ratings were done
+    :param ratings: a collection of all ratings
+    :return: returns an integer.
+    """
+    counter = 0
+    for rating in ratings:
+        if rating['movieid'] is movieID:
+            counter += 1
+    return counter
+
+def how_many_X_and_Y(movieX_ID, movieY, ratings):
+    """
+    Computes how many users rated movie X and movie Y as well.
+    :param movieX_ID: a movie ID
+    :param movieY: another movie ID
+    :param ratings: a collection of all ratings
+    :return: returns an integer.
+    """
