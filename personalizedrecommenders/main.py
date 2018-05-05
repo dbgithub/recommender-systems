dea04a7a9f7abc280355db222c4b7feac98ac17d
Aitor DB <aitor.db@opendeusto.es>
2018-05-05 02:42:04 +0200

    Main and Utils files formated or cleaned for the new project. Main and most relevant methods declared in Main.

--- personalizedrecommenders/main.py
+++ personalizedrecommenders/main.py
@@ -1,177 +1,103 @@
 """
-This file is the main file. The interaction with the recommender service starts here.
+This file is the main file. Main methods for personalized recommender system can be found here.
 
 """
 
 __author__ = "Aitor De Blas Granja"
 __email__ = "aitor.deblas@ugent.be"
 
-import sugestio
-from sugestio import Consumption, Item, User
 from numpy import mean, median, unique
 import data
 import utils
 
-# ACCOUNT = 'sandbox'
-# SECRET = 'demo'
-ACCOUNT = 's2018debla'
-SECRET = 'Dzkux4G9k1AZzVCA'
 MOVIE_PICKLE_LOCATION = "movies_pickle_10-04-2018--18-59-27.pkl"
 RATINGS_PICKLE_LOCATION = "ratings_pickle_05-04-2018--20-02-35.pkl"
 LOG_STATUS = True
 
-# Declare Sugestio client (using already existing Sugestio library for python):
-# Info: https://github.com/sugestio/sugestio-python
-SUGESTIOCLIENT = sugestio.Client(ACCOUNT, SECRET)
-
-
-def submit_metadata_single_movie(movieID, metadata):
+def calculate_pearson_correlation(common_ratings, userAid, userUid):
     """
-    Submits information about the movie with ID passed by parameter.
-    Different data fields can be specified. Request method is POST.
-    :param movieID: int number of the movie identifier
-    :param metadata: a dictionary containing the metadata about the movie to be added
-    :return: int status
+    Calculates Pearson correlation.
+    :param common_ratings: a list of common ratings rated by either user A and user U
+    :param userAid: int number of the id of user A
+    :param userUid: int number of the id of user B
+    :return: float number, weight of Pearson correlation
     """
-    item = Item(movieID)
-    item.title = metadata['title']
-    genres = metadata['genre'].split('|')
-    for genre in genres:
-        item.category.append(genre)
-    status, raw = SUGESTIOCLIENT.add_item(item)
-    if status == 200 or status == 202:
-        if LOG_STATUS is True:
-            print "[",status,"]: Movie metadata submitted successfully!"
-    else:
-        print "[",str(status),"]: Something went wrong."
-    return status
+    pass
 
-
-def submit_rating_single_movie(movieID, rating):
+def calculate_mean_ratings(userID):
     """
-    Submits a rating (consumption) for a certain movie.
-    Different data fields can be specified. Request method is POST.
-    :param movieID: int number of the movie identifier
-    :param rating: the rating object to be submitted
-    :return: int status
+    Calculates the average of the ratings of the user given by parameter
+    :param userID: int number of the id of the user
+    :return: float number, average of the ratings
     """
-    con = Consumption(rating['userid'], movieID)
-    con.type = "RATING"
-    con.detail = "STAR:5:0.5:" + str(float(rating['rating']))
-    con.date = rating['timestamp']
-    status, raw = SUGESTIOCLIENT.add_consumption(con)
-    if status == 200 or status == 202:
-        if LOG_STATUS is True:
-            print "[",status,"]: Rating for single movie submitted/updated successfully!"
-    else:
-        print "[",str(status),"]: Something went wrong."
-    return status
 
-
-def update_rating_single_movie(movieID, rating):
+def calculate_centered_mean_rating_user(itemID, userID, avgRatingUser):
     """
-    Updates a rating (consumption) for a certain movie.
-    Different fields can be specified. Request method is POST.
-    :param movieID: int number of the movie identifier
-    :param rating: the rating object to be updated
-    :return: int status
+    Calculates the centered mean rating of user 'userID' over ALL his ratings
+    :param itemID: int number of the item
+    :param userID: int number of the user
+    :param avgRatingUser: mean ratings of the user
+    :return: float number
     """
-    # In order to differentiate between submit and update I declared two methods, but they eventually
-    # do the same thing behind the scenes. The reason is to
-    return submit_rating_single_movie(movieID, rating)
 
-
-def topN_recommendations_user(userID, N = None):
+def calculate_significance_weighing(userAid, userUid):
     """
-    Computes the topN recommendations for this user based on the gathered data and computed tasks
-    by the recommender service.
-    Request method is GET.
-    :param userID: int number of the user identifier
-    :param N: amount of recommendation to be retrieved
-    :return: int status and a list of recommendations
+    Calculates the significance weighing
+    :param userAid: int number of user A
+    :param userUid: int number of user B
+    :return: float number
     """
-    # Check if N is none, raise an error if yes.
-    try:
-        if N is None:
-            raise AttributeError
-        if not isinstance(N, int):
-            raise ValueError
-    except AttributeError:
-        print "[Error] Parameter value missing. N is none"
-    except ValueError:
-        print "[Error] N is not an int, it should be an int number"
-    status, recommendations = SUGESTIOCLIENT.get_recommendations(userID, limit=N)
-    if status == 200 or status == 202:
-        if LOG_STATUS is True:
-            print "[",status,"]: top N recommendations retrieved successfully!"
-    else:
-        print "[",str(status),"]: Something went wrong."
-    return status, recommendations
 
-
-def rating_history_user(userID):
+def intersection_ratings_users(ratingsA, ratingsU):
     """
-    Obtains the consumption data (ratings) of a certain user passed by parameter.
-    It basically retrieves all the consumption data related to that user.
-    :param userID: int number of the user identifier
-    :return: int status and a list of consumptions
+    Intersects both ratings sets to find the common ratings by both users
+    :param ratingsA: list of ratings of user A
+    :param ratingsU: list of ratings of user B
+    :return: list of ratings
     """
-    status, consumptions = SUGESTIOCLIENT.get_user_consumptions(userID)
-    if status == 200 or status == 202:
-        if LOG_STATUS is True:
-            print "[",status,"]: user history retrieved successfully!"
-    else:
-        print "[",str(status),"]: Something went wrong."
-    return status, consumptions
 
-
-def get_metadata_movie(movieID):
+def rating_prediction_user(userAid,itemID, neighborsIDs):
     """
-    Retrieves the metadata related to a movie which was previously submitted.
-    :param movieID: int number of the movie identifier
-    :return: metadata of the movie. It returns a single item containing tuples accessed as attributes of an object
+    Calculates the rating prediction for user A and for item 'itemID'
+    based on the nearest neighbors.
+    :param userAid: int number of user A
+    :param itemID: int number of the item
+    :param neighborsIDs: list of the nearest neighbors IDs
+    :return: float number
     """
-    status, movie = SUGESTIOCLIENT.get_item(movieID)
-    if status == 200 or status == 202:
-        if LOG_STATUS is True:
-            print "[",status,"]: movie retrieved successfully!"
-    else:
-        print "[",str(status),"]: Something went wrong."
-    return status, movie
 
-
-def submit_movies_metadata_bulk(movies):
+def top_k_most_similar_neighbors(userID, itemID):
     """
-    Submits in bulk-fashion all movies passed by parameter.
-    :param movies: a collection of movies to be submitted to the recommender service.
-    :return:
+    Retrieves the most similar neighbor of user 'userID' with respect to item 'itemID'
+    :param userID: int number of the user
+    :param itemID: int number of the item
+    :return: list of neighbors IDs
     """
-    items = []
-    for movie in movies:
-        item = Item(movie['id'])
-        item.title = movie['title']
-        genres = movie['genre'].split('|')
-        for genre in genres:
-            item.category.append(genre)
-        items.append(item)
-    SUGESTIOCLIENT.add_items(items)
 
-
-def submit_movies_ratings_bulk(ratings):
+def compute_cosine_similarity(itemID_i,itemID_j):
     """
-    Submits in bulk-fashion all movies passed by parameter.
-    :param ratings: a collection of ratings to be submitted to the recommender service.
-    :return:
+    Computes cosine similarity between item i and item j
+    :param itemID_i: int number of item i
+    :param itemID_j: int number of item j
+    :return: float number
     """
-    consumptions = []
-    for rating in ratings:
-        con = Consumption(rating['userid'], rating['movieid'])
-        con.type = "RATING"
-        con.detail = "STAR:5:0.5:" + str(float(rating['rating']))
-        con.date = rating['timestamp']
-        consumptions.append(con)
-    SUGESTIOCLIENT.add_consumptions(consumptions)
 
+def calculate_centered_mean_rating_item(itemID, userID, avgRatingItem):
+    """
+    Calculates centered mean rating of the item 'itemID' for all users.
+    :param itemID: int number of the item
+    :param userID: int number of the user
+    :param avgRatingItem: mean ratings of item 'itemID'
+    :return: float number
+    """
+
+def rating_prediction_item(itemID_i, userID):
+    """
+    Calculates the rating prediction for user 'userID' and item
+    :param itemID_i: int number of the item
+    :param userID: int number of the user
+    :return: float number
+    """
 
 def main():
     # Load data and parse it:
@@ -185,167 +111,9 @@ def main():
     ratings_pkl = data.load_pickle(RATINGS_PICKLE_LOCATION)
     print "len(movies_pkl): ", len(movies_pkl)
     print "len(ratings_pkl): ", len(ratings_pkl)
-    # submit_movies_metadata_bulk(movies_pkl.values())
-    # submit_movies_ratings_bulk(ratings_pkl)
 
     # # Question 1:
-    print "Question 1: rating of movie 1125 by user 289."
-    # s, rating = utils.get_rating(289, 1125)
-    # print "\t\t| rating =", utils.decode_stars(rating[0].detail)
-    # print "\t\t| date & time =", rating[0].date
-    # s, metadata = get_metadata_movie(1125)
-    # print "\t\t| title =", metadata.title
-    # print "\t\t| genre(s) =",
-    # for genre in metadata.category:
-    #     print genre,
-    # print ""  # this print is just for readability
-    # # Question 3:
-    print "\nQuestion 3: rating history user 249."
-    # s, history = rating_history_user(249)
-    # rating_stars = []
-    # for rating in history:
-    #     s, metadata = get_metadata_movie(rating.itemid)
-    #     genres = ""
-    #     for genre in metadata.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rating.itemid, ":: Title =", metadata.title, ":: Genre(s) =", genres, ":: Rating =", utils.decode_stars(rating.detail)
-    #     rating_stars.append(utils.decode_stars(rating.detail))
-    # print "\t\t| Mean =", mean(rating_stars)
-    # print "\t\t| Median =", median(rating_stars)
-    # print "\t\t| Uniqueness =", dict(zip(*unique(rating_stars, return_counts=True)))
-    # # Question 4:
-    print "\nQuestion 4: top five collaborative filtering recommendations user 249."
-    # s, top5 = topN_recommendations_user(249, 5)
-    # for rec in top5:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # # Question 5:
-    print "\nQuestion 5: rating history user 35."
-    # s, history = rating_history_user(35)
-    # rating_stars = []
-    # for rating in history:
-    #     s, metadata = get_metadata_movie(rating.itemid)
-    #     genres = ""
-    #     for genre in metadata.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rating.itemid, ":: Title =", metadata.title, ":: Genre(s) =", genres, ":: Rating =", utils.decode_stars(rating.detail)
-    #     rating_stars.append(utils.decode_stars(rating.detail))
-    # print "\t\t| Mean =", mean(rating_stars)
-    # print "\t\t| Median =", median(rating_stars)
-    # print "\t\t| Uniqueness =", dict(zip(*unique(rating_stars, return_counts=True)))
-    # # Question 6:
-    print "\nQuestion 6: top five collaborative filtering recommendations user 35."
-    # s, top5 = topN_recommendations_user(35, 5)
-    # for rec in top5:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # # Question 8:
-    print "\nQuestion 8: top five content based recommendations user 249."
-    # s, top5 = topN_recommendations_user(249, 5)
-    # for rec in top5:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # # Question 9:
-    print "\nQuestion 9: top five content based recommendations user 35."
-    # s, top5 = topN_recommendations_user(35, 5)
-    # for rec in top5:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # Question 10a:
-    print "\nQuestion 10a: additional raitings for new user 1000."
-    # additional_ratings = [
-    #     {'userid': 1000, 'movieid': 1590, 'rating': 4.0, 'timestamp': 1476640644},
-    #     {'userid': 1000, 'movieid': 1196, 'rating': 4.5, 'timestamp': 1476640644},
-    #     {'userid': 1000, 'movieid': 4878, 'rating': 4.0, 'timestamp': 1476640644},
-    #     {'userid': 1000, 'movieid': 589, 'rating': 4.5, 'timestamp': 1476640644},
-    #     {'userid': 1000, 'movieid': 480, 'rating': 4.5, 'timestamp': 1476640644}
-    # ]
-    # submit_movies_ratings_bulk(additional_ratings) # Make sure to execute this once and at the correct moment
-    # Question 10b:
-    print "\nQuestion 10b: rating history user 1000."
-    # s, history = rating_history_user(1000)
-    # for rating in history:
-    #     s, metadata = get_metadata_movie(rating.itemid)
-    #     genres = ""
-    #     for genre in metadata.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rating.itemid, ":: Title =", metadata.title, ":: Genre(s) =", genres, ":: Rating =", utils.decode_stars(rating.detail)
-    # Question 12:
-    print "\nQuestion 12: top 10 collaborative filtering recommendations user 1000."
-    # s, top10 = topN_recommendations_user(1000, 10)
-    # for rec in top10:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # Question 14:
-    print "\nQuestion 14: top 10 content based recommendations user 1000."
-    # s, top10 = topN_recommendations_user(1000, 10)
-    # for rec in top10:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # Question 15:
-    print "\nQuestion 15: additional rating by user 1000."
-    # s = submit_rating_single_movie(6587, {'userid': 1000, 'movieid': 6587, 'rating': 1.0, 'timestamp': 1476640644})
-    # s, metadata = get_metadata_movie(6587)
-    # genres = ""
-    # for genre in metadata.category:
-    #     if genres is "":
-    #         genres = "{0}".format(genre)
-    #     else:
-    #         genres = "{0}, {1}".format(genres, genre)
-    # print "\t\t| Movie title =", metadata.title, ":: Genre(s) =", genres
-    # Question 17:
-    print "\nQuestion 17: top 10 content based recommendations user 1000."
-    # s, top10 = topN_recommendations_user(1000, 10)
-    # for rec in top10:
-    #     genres = ""
-    #     for genre in rec.item.category:
-    #         if genres is "":
-    #             genres = "{0}".format(genre)
-    #         else:
-    #             genres = "{0}, {1}".format(genres, genre)
-    #     print "\t\t| Movie id =", rec.item.id, ":: Title =", rec.item.title, ":: Genre(s) =", genres, ":: Score =", rec.score
-    # Question 18:
-    print "\nQuestion 18: delete all consumptions user 1000."
-    # utils.delete_all_consumptions_user(1000)
+    # print "Question 1: rating of movie 1125 by user 289."
 
 
 def test():
@@ -356,38 +124,11 @@ def test():
     """
     print "HelloWorldTEST!"
 
-    # -------------------------------
-    # Testing library against Sugestio API:
-
-    # client = sugestio.Client(ACCOUNT, SECRET)
-    # status, content = client.get_recommendations(1, 5)
-    # if status == 200:
-    #     print("Title\tScore")
-    #     for recommendation in content:
-    #         print(recommendation.item.title + "\t" + str(recommendation.score))
-    # else:
-    #     print("server response code:", status)
-
-    # -------------------------------
-    # Testing own implemented methods:
-
     # Data:
     movies_pkl = data.load_pickle(MOVIE_PICKLE_LOCATION)
     ratings_pkl = data.load_pickle(RATINGS_PICKLE_LOCATION)
     # Testing methods:
-    submit_metadata_single_movie(83829, movies_pkl['83829'])
-    submit_rating_single_movie(8, ratings_pkl[3500])
-    update_rating_single_movie(8, ratings_pkl[200])
-    s, recommendations = topN_recommendations_user(1, N=5)
-    for i, rec in enumerate(recommendations):
-        print "\t\t Recommendation#",i,": ", rec
-    s, consumptions = rating_history_user(1)
-    for i, con in enumerate(consumptions):
-        print "\t\t Consumptions#",i,": ", con
-    s, mov = get_metadata_movie(2)
-    print "Movie details: ", mov
-    submit_movies_metadata_bulk(movies_pkl.values())
-    submit_movies_ratings_bulk(ratings_pkl)
+    # ...
 
 
 if __name__ == '__main__':
--- personalizedrecommenders/utils.py
+++ personalizedrecommenders/utils.py
@@ -8,44 +8,6 @@ For instance, statistics methods, subroutines for main methods in main, etc.
 __author__ = "Aitor De Blas Granja"
 __email__ = "aitor.deblas@ugent.be"
 
-from main import SUGESTIOCLIENT, LOG_STATUS
 
-
-def delete_all_consumptions_user(userID):
-    """
-    Deletes all consumptions made by user identified by userID.
-    :param userID: int number of the user identifier
-    :return:
-    """
-    s, response = SUGESTIOCLIENT.delete_user_consumptions(userID)
-    if s == 200 or s == 202:
-        if LOG_STATUS is True:
-            print "[",s,"]: All consumptions of user ",userID," were deleted successfully!"
-    else:
-        print "[",str(s),"]: Something went wrong."
-
-
-def get_rating(userID, movieID):
-    """
-    Retrieves the consumption from the recommender service provided the user ID.
-    :param userID: int number of the user identifier
-    :param movieID: int number of the movie identifier
-    :return: the rating. It returns a list with a single item containing tuples accessed as attributes of an object
-    """
-    s, rating = SUGESTIOCLIENT.get_user_consumptions(userID, movieID)
-    if s == 200 or s == 202:
-        if LOG_STATUS is True:
-            print "[", s, "]: Movie rating retrieved successfully!"
-        return s, rating
-    else:
-        print "[", str(s), "]: Something went wrong."
-        return s
-
-
-def decode_stars(detailfield):
-    """
-    Decodes the stars rating from the provided parameter
-    :param detailfield: string representation of the rating for a certain movie
-    :return: float of the rating
-    """
-    return float(detailfield.split(':')[3])
+def helloworld():
+    pass
